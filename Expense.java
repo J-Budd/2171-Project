8dbf0c24f7724145301319ed7a67833e744d0476
danielle-brooks <danielle04brooks@gmail.com>
2025-04-11 11:45:38 -0500

    Create Expense.java

---
+++ Expense.java
@@ -0,0 +1,51 @@
+
+package expense;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class Expense implements Serializable {
+    private double amount;
+    private String description;
+    private String type;
+    private Date date;
+
+    public Expense(double amount, String description, String type) {
+        this.amount = amount;
+        this.description = description;
+        this.type = type;
+        this.date = new Date();
+    }
+
+    public double getAmount() {
+    	return amount; 
+    	}
+    
+    public void setAmount(double amount) {
+    	this.amount = amount; 
+    	}
+
+    public String getDescription() { 
+    	return description; 
+    	}
+    
+    public void setDescription(String description) { 
+    	this.description = description; 
+    	}
+
+    public String getType() { 
+    	return type; 
+    	}
+    public void setType(String type) { 
+    	this.type = type; 
+    	}
+
+    public Date getDate() { 
+    	return date; 
+    	}
+    public void setDate(Date date) { 
+    	this.date = date;
+    	
+    }
+}
